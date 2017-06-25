c389c9b8d82c2f2d75bab60e0cf921154c11cfd0
Alvin Wan <hi@alvinwan.com>
2017-06-24 22:59:48 -0700

    rudimentary working web interface with motion detection

---
+++ desktop/README.md
@@ -0,0 +1,15 @@
+# MotioSecure Desktop
+
+> This project was created for Python3.6. Since the project uses typecasting, it does not support Python2. It may not support older versions of python3.x even.
+
+To install, install Python dependencies
+
+```
+pip install -r requirements.txt
+```
+
+To launch, just run the Python file from this directory.
+
+```
+FLASK_APP=run.py flask run
+```
---
+++ desktop/run.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+import time
+import threading
+
+from flask import Flask
+from flask import url_for
+from sklearn.decomposition import TruncatedSVD
+from collections import deque
+
+import websockets
+import asyncio
+import datetime
+
+app = Flask(__name__)
+
+motion_detected = 'False'
+DEQUE_LENGTH = 10
+THRESHOLD = 0.05
+
+@app.route("/")
+def index():
+    return '<a href="%s">Start monitoring"</a>' % url_for('monitor')
+
+
+@app.route("/monitor")
+def monitor():
+    t = threading.Thread(target=watch)
+    t.start()
+    return """
+    <!DOCTYPE html>
+<html>
+    <head>
+        <title>WebSocket demo</title>
+    </head>
+    <body>
+        <p>Monitoring started
+        <ul>
+            <li>Motion Detected: <span id="detected"></span></li>
+        </ul>
+        <script>
+            window.onload = function() {
+                var ws = new WebSocket("ws://127.0.0.1:5678/");
+                var status = document.getElementById('detected');
+                ws.onmessage = function (event) {
+                    status.innerHTML = event.data;
+                    console.log(event.data);
+                };
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+def watch():
+    global motion_detected
+    capture = cv2.VideoCapture(0)
+    if capture is None or not capture.isOpened():
+        print('Warning: unable to open video source: ', source)
+
+    images = deque(maxlen=DEQUE_LENGTH)
+    svd = TruncatedSVD(n_components=5)
+    while True:
+        _, image = capture.read()
+        if len(images) > DEQUE_LENGTH - 1:
+            difference = np.mean(image - images[-DEQUE_LENGTH+1], axis=2)
+            svd.fit(difference)
+            total_explained_variance = svd.explained_variance_ratio_.sum()
+            motion_detected = str(total_explained_variance > THRESHOLD)
+        images.append(image)
+
+def start_socket():
+    start_server = websockets.serve(send_detections, '127.0.0.1', 5678)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+async def send_detections(websocket, path):
+    global motion_detected
+    while True:
+        await websocket.send(motion_detected)
+        await asyncio.sleep(0.1)
+
+t = threading.Thread(target=start_socket)
+t.start()
