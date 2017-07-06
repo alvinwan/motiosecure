bc71c36e7aa6f35eb57a34e54093f056f150726d
Alvin Wan <hi@alvinwan.com>
2017-07-05 23:26:08 -0700

    new version spec

--- desktop/packager.sh
+++ desktop/packager.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+export PLATFORM=darwin
+export ELECTRON_VERSION=1.6.11
+
 # Usage: ./packager.sh <team ID>
 # Instructions taken from https://github.com/electron-userland/electron-osx-sign/wiki/Packaging-and-Submitting-an-Electron-App-to-the-Mac-App-Store
 pyinstaller motiosecure/api.py --distpath dist --hidden-import sklearn.neighbors.typedefs
-electron-packager . "MotioSecure" --app-bundle-id=com.alvinwan.mac.MotioSecure --app-version=0.1.2 --build-version=0.1.2 --electron-version=1.6.11 --icon=Icon.icns --overwrite
-electron-osx-sign "MotioSecure-mas-x64/MotioSecure.app" --verbose
-electron-osx-sign "MotioSecure-mas-x64/MotioSecure.app" {MotioSecure-mas-x64/MotioSecure.app/Contents/Resources/app/dist/api/*.so,MotioSecure-mas-x64/MotioSecure.app/Contents/Resources/app/dist/api/**/*.so} --verbose
-electron-osx-sign "MotioSecure-mas-x64/MotioSecure.app" --identity="3rd Party Mac Developer Application: Alvin Wan ($1)" --verbose
-electron-osx-flat "MotioSecure-mas-x64/MotioSecure.app" --verbose
-electron-osx-flat "MotioSecure-mas-x64/MotioSecure.app" --identity="3rd Party Mac Developer Installer: Alvin Wan ($1)" --verbose
+electron-packager . "MotioSecure" --app-bundle-id=com.alvinwan.mac.MotioSecure --platform=$PLATFORM --app-version=0.1.2 --build-version=0.1.2 --electron-version=$ELECTRON_VERSION --icon=Icon.icns --overwrite
+electron-osx-sign "MotioSecure-$PLATFORM-x64/MotioSecure.app" --verbose
+electron-osx-sign "MotioSecure-$PLATFORM-x64/MotioSecure.app" {MotioSecure-$PLATFORM-x64/MotioSecure.app/Contents/Resources/app/dist/api/*.so,MotioSecure-$PLATFORM-x64/MotioSecure.app/Contents/Resources/app/dist/api/**/*.so} --verbose
+electron-osx-sign "MotioSecure-$PLATFORM-x64/MotioSecure.app" --identity="3rd Party Mac Developer Application: Alvin Wan ($1)" --verbose --no-gatekeeper-assess
+electron-osx-flat "MotioSecure-$PLATFORM-x64/MotioSecure.app" --verbose
+electron-osx-flat "MotioSecure-$PLATFORM-x64/MotioSecure.app" --identity="3rd Party Mac Developer Installer: Alvin Wan ($1)" --verbose --no-gatekeeper-assess
